380d93f9d0c3d2b7dc82bb51e7f607f2a8d1f942
Msn69 <119772264+Msn69@users.noreply.github.com>
2022-12-04 00:40:57 -0600

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -3,8 +3,8 @@ const params = new URLSearchParams(location.search);
 const num1 = params.get('num')
 const num2 = params.get('num2')
 const type = params.get('type')
-let answer = parseFloat(num1) ftype parseFloat(num2);
-document.getElementById("window").innerHTML = browser;
+let answer = parseFloat(num1) type parseFloat(num2);
+document.getElementById("solution").innerHTML = answer;
 }
 function full() {
 document.getElementById("window").requestFullscreen()
