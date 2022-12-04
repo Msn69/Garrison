d912c375ade3bfdd93586a0fb10aa0b68c2b8114
Msn69 <119772264+Msn69@users.noreply.github.com>
2022-12-04 01:09:29 -0600

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -3,20 +3,16 @@ var x = prompt("+,-,×,÷")
 var y = prompt("num:")
 var z = prompt(num 2:")
 if (x == "+") {
-var a = +
-var b = parseFloat(y) a parseFloat(z)
+var b = parseFloat(y) + parseFloat(z)
 }
 else if (x == "-") {
-var a = -
-var b = parseFloat(y) a parseFloat(z)
+var b = parseFloat(y) - parseFloat(z)
 }
 else if (x == "×") {
-var a = *
-var b = parseFloat(y) a parseFloat(z)
+var b = parseFloat(y) * parseFloat(z)
 }
 else if (x == "÷") {
-var a = ÷
-var b = parseFloat(y) a parseFloat(z)
+var b = parseFloat(y) / parseFloat(z)
 }
-document.getElementById("solution").innerHTML = b;
+alert(b)
 }
